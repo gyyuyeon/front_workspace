bd96f2580ec213fc809873f6d1df2eb9f8e3eacb
gyyuyeon <gyyuyeon@gmail.com>
2024-02-27 08:55:10 +0900

    new

--- js040_select.js
+++
@@ -1,9 +0,0 @@
-document.frm.onsubmit = () => {
-  let optFruit = document.frm.fruit;
-  console.log(optFruit);
-
-  let optSelect = optFruit.selectedIndex + ' ' + optFruit.value;
-  console.log(`${optSelect}`);
-
-  return false;
-};
--- js043_checked.js
+++
@@ -1,9 +0,0 @@
-document.carZone.onsubmit = () => {
-  let opt1 = document.carZone.opt1;
-  if (!opt1.checked) {
-    alert('ok00');
-    let input = document.getElementsByTagName('input')[0];
-    input.innerText = 25000;
-    return false;
-  }
-};
